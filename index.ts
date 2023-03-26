c2a4767d3bc571d07038c55edf2e301bf7971708
Igor Savin <iselwin@gmail.com>
2023-03-26 14:59:38 +0300

    Export error handler

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export { ChatGPTClient } from "./src/ChatGPTClient";
 export { ConversationContext } from "./src/ConversationContext";
 export type { PromptParams } from "./src/ChatGPTClient";
+export type { ErrorHandler } from "./src/Logger";
